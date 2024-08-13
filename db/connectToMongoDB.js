c558d19108ef8be2fab50ed18c75ac3df0353db1
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-13 18:43:47 +0600

    organized connectToMongoDB.js by adding comments and gaps

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -2,10 +2,14 @@ import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
     try {
+        // Connect to MongoDB using the URI from environment variables
         await mongoose.connect(process.env.MONGO_DB_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
+        // Log the error and provide a descriptive message
         console.log('Error connecting to MongoDB:', error);
+        // Optionally, you could also exit the process in case of failure
+        process.exit(1);
     }
 }
 
