601d46584e57a11937fc99e37d93ac4001bb6378
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-13 18:41:58 +0600

    organized user.controller.js by adding comments and gaps

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,13 +2,17 @@ import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
+        // Extract the ID of the logged-in user from the request
         const loggedInUserId = req.user._id;
 
-        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
-        
+        // Find users excluding the logged-in user and select fields except for password
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+        // Respond with the list of filtered users
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.log('Error in getUsersForSidebar: ', error);
-        res.status(500).json({error: 'Internal Server Error'});
+        // Log the error and respond with a 500 status
+        console.error('Error in getUsersForSidebar:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+};
