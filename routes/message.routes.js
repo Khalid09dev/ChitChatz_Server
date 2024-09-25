56225283e2fd8db48f598898c69bd19106baccde
khalid09dev <mdkhalidhossen@hotmail.com>
2024-09-25 22:05:33 +0600

    added s3 file upload functionality using multer

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,10 +1,27 @@
 import express from 'express';
 import { getMessages, sendMessage } from '../controllers/message.controller.js';
 import privateRoute from '../middlewares/privateRoute.js';
+import multer from 'multer';
+import multers3 from 'multer-s3';
+import s3 from '../config/awsS3.js';
+
+
+const upload = multer({
+    storage: multers3({
+        s3: s3,
+        bucket: process.env.AWS_S3_BUCKET,
+        metadata: (req, file, cb) => {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: (req, file, cb) => {
+            cb(null, Date.now().toString() + '-' + file.originalname)
+        }
+    })
+})
 
 const router = express.Router();
 
 router.get('/:id/:jwt', privateRoute, getMessages);
-router.post('/send/:id', privateRoute, sendMessage);
+router.post('/send/:id', privateRoute, upload.array('files'), sendMessage);
 
 export default router;
