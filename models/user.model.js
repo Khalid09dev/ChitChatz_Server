40711474c54d19081e9b8e2c8278babd5d94a969
khalid09dev <mdkhalidhossen@hotmail.com>
2024-10-01 12:54:25 +0600

    added new schema property named profile in user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 6
+    }, 
+    profile: {
+        type: String,
+        required: true
     }
     //createdAt and updatedAt
 }, {timestamps: true});
